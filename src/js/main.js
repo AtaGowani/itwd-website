7a251e0dbc7df2a53d2f66aa46431d0c7857f31d
Ata Gowani <atagowani@gmail.com>
2017-07-30 10:45:52 -0500

    Remove pre-registration opening content

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-var now = new Date();
-var releaseDate = new Date("Fri Jul 28 2017 12:45")
-
-if (now >= releaseDate) {
-  document.getElementsByClassName('typeform-share')[0].classList = "typeform-share button btn btn-primary btn-lg"
-  document.getElementsByClassName('registration-time')[0].style = "display: none;"
-}

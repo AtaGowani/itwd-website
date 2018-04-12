2cad071ad15828504815c206229da22c6b2743d0
Ata Gowani <atagowani@gmail.com>
2018-04-12 16:47:33 -0500

    Remove AOS animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,25 @@
-var app = angular.module('itwd', ['ui.router'])
+var app = angular.module('itwd', ['ui.router', 'ngAnimate'])
+
+app.animation('.slide', [function() {
+  return {
+    // make note that other events (like addClass/removeClass)
+    // have different function input parameters
+    enter: function(element, doneFn) {
+      jQuery(element).fadeIn(1000, doneFn);
+
+      // remember to call doneFn so that AngularJS
+      // knows that the animation has concluded
+    },
+
+    move: function(element, doneFn) {
+      jQuery(element).fadeIn(1000, doneFn);
+    },
+
+    leave: function(element, doneFn) {
+      jQuery(element).fadeOut(1000, doneFn);
+    }
+  }
+}]);
 
 app.config(function($stateProvider, $urlRouterProvider){
 

96d0f1a4e5e417b7a513366a5d2bd55f207659b0
Ata Gowani <atagowani@gmail.com>
2018-04-16 00:44:44 -0500

    Fix routes for static files

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+app.use('/src', express.static(path.join(__dirname, 'src')));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
